49d4f368b14286ad9454cdf1bb5d6fd50ea19062
joaobuga35 <joaobuga35@gmail.com>
2023-09-11 17:59:40 -0300

    first commit

---
+++ src/test/java/com/jlbooks/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jlbooks.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
